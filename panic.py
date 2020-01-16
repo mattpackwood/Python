d099358ed06b0ab6bad4f2db2a28ce548b59e9b5
Matt Packwood <mattpackwood@gmail.com>
2020-01-16 15:20:05 -0500

    New file

---
+++ panic.py
@@ -0,0 +1,11 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+for i in range(4):
+    plist.pop()
+plist.pop(0)
+plist.remove("'")
+plist.extend([plist.pop(), plist.pop()])
+plist.insert(2,plist.pop(3))
+new_phrase = "".join(plist)
+print(plist)
+print(new_phrase)
