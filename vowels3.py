d6b3b9bd77a4ad593e46617451090ded31d03407
Matt Packwood <mattpackwood@gmail.com>
2020-05-24 18:17:07 -0400

    Changed input text

--- vowels3.py
+++ vowels3.py
@@ -1,5 +1,5 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
-word = input("Provide a word to search for vowels: ")
+word = input("Provide a word or words to search for vowels: ")
 found = []
 for letter in word:
     if letter in vowels:
